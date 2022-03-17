d5e9955be62efb1b9104bf2e80096eeb03dfb15b
Eunseong, Yeo <e7838752@gmail.com>
2022-03-17 17:15:05 +0900

    🚚 move: change directory from components to routes

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,16 +1,14 @@
-import React, {useState} from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from '../components/Home';
-import Auth from '../components/Auth';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "routes/Home";
+import Auth from "routes/Auth";
 
-const AppRouter = ({isLoggedIn}) => (
-    <Router>
-      <Routes>
-        <Route path='/'  element={isLoggedIn ? <Home/> : <Auth/>}/>
-      </Routes>
-    </Router>
-  )
-
-
+const AppRouter = ({ isLoggedIn }) => (
+  <Router>
+    <Routes>
+      <Route path="/" element={isLoggedIn ? <Home /> : <Auth />} />
+    </Routes>
+  </Router>
+);
 
 export default AppRouter;

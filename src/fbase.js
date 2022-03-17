7d85e5af2c51054ec4347323e2dde7dd26665fac
Eunseong, Yeo <e7838752@gmail.com>
2022-03-17 16:58:11 +0900

    🚚 rename: move firebase to fbase
    rename firebase file to avoid conflict

--- src/fbase.js
+++ src/fbase.js
@@ -1,4 +1,5 @@
-import { initializeApp } from "firebase/app"
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -6,7 +7,9 @@ const firebaseConfig = {
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SEMDER_ID,
-  appId: process.env.REACT_APP_APP_ID
+  appId: process.env.REACT_APP_APP_ID,
 };
 
-export default initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+
+export const authService = getAuth();

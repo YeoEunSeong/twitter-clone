62e60eeec88968f9f8b92000d77219d37e6af2bb
Eunseong, Yeo <e7838752@gmail.com>
2022-03-17 18:48:15 +0900

    🚚 move: move Router component from routes to components

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "routes/Home";
 import Auth from "routes/Auth";
+import Profile from "routes/Profile";
 
 const AppRouter = ({ isLoggedIn }) => (
   <Router>
     <Routes>
       <Route path="/" element={isLoggedIn ? <Home /> : <Auth />} />
+      <Route path="/profile" element={<Profile />} />
     </Routes>
   </Router>
 );
--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import AppRouter from "routes/AppRouter";
+import AppRouter from "components/AppRouter";
 import { authService } from "fbase";
 import { onAuthStateChanged } from "firebase/auth";
 

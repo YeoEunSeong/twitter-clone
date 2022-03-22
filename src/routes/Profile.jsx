43fa6451d742a378d7950768015c21275b5b1974
Eunseong, Yeo <e7838752@gmail.com>
2022-03-22 17:14:21 +0900

    ✨ feat: create logout

--- src/routes/Profile.jsx
+++ src/routes/Profile.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { logout } from "fbase";
+import { useNavigate } from "react-router-dom";
 
 const Profile = (props) => {
-  return <button>Sign Out</button>;
+  // Add logic that users who logged in can see this page
+  const navigate = useNavigate();
+  const onClick = () => {
+    logout();
+    navigate("/");
+  };
+
+  return <button onClick={onClick}>Sign Out</button>;
 };
 
 export default Profile;

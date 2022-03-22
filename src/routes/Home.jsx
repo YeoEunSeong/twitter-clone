438f01f9fbceaf5337e22359bb282aebd20d2398
Eunseong, Yeo <e7838752@gmail.com>
2022-03-22 17:14:34 +0900

    ✨ feat: create tweet form

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useState } from "react";
 
-const Home = (props) => (
-      <h1>Home</h1>
+const Home = (props) => {
+  const [tweet, setTweet] = useState("");
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+    setTweet(value);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(tweet);
+  };
+  return (
+    <>
+      <h1> Home</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          type="text"
+          maxLength={120}
+          placeholder="What's on your mind?"
+        />
+        <button>submit</button>
+      </form>
+    </>
   );
+};
 
 export default Home;

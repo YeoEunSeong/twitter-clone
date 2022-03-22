bdfa45e3806cf4b161189a9d18fce559559d8989
Eunseong, Yeo <e7838752@gmail.com>
2022-03-22 17:14:00 +0900

    ✨ feat: Add navigation

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -3,9 +3,11 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "routes/Home";
 import Auth from "routes/Auth";
 import Profile from "routes/Profile";
+import Navigation from "components/Navigation";
 
 const AppRouter = ({ isLoggedIn }) => (
   <Router>
+    {isLoggedIn && <Navigation />}
     <Routes>
       <Route path="/" element={isLoggedIn ? <Home /> : <Auth />} />
       <Route path="/profile" element={<Profile />} />
